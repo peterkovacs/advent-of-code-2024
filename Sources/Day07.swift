89c1f414709e321c2ce28c561ad4d11bb2a709c7
Peter Kovacs <peter@kovax.org>
2024-12-07 13:28:21 -0500

    Day 7: optimizations

--- Sources/Day07.swift
+++ Sources/Day07.swift
@@ -1,5 +1,8 @@
 import ArgumentParser
+import Atomics
+import Dispatch
 import Parsing
+import _math
 
 struct Day7: ParsingCommand {
     static var parser: some Parser<Substring.UTF8View, [(Int, [Int])]> {
@@ -16,7 +19,7 @@ struct Day7: ParsingCommand {
         let input = try parsed()
 
         let (p2input, p1input) = input.partitioned { (target, operands) in
-            solve(target: target, operands: operands[1...], accumulator: operands[0])
+            solve(target: target, operands: operands[...].reversed())
         }
 
         let part1 = p1input
@@ -25,60 +28,48 @@ struct Day7: ParsingCommand {
 
         print("Part 1", part1)
 
-        let part2 = p2input
-            .filter { (target, operands) in solve2(target: target, operands: operands[1...], accumulator: operands[0]) }
-            .map(\.0)
-            .reduce(0, +)
+        let part2 = ManagedAtomic<Int>(0)
 
-        print("Part 2", part1 + part2)
+        DispatchQueue.concurrentPerform(iterations: p2input.count) { index in
+            let (target, operands) = p2input[index]
+            if solveWithConcatenation(target: target, operands: operands[...].reversed()) {
+                part2.wrappingIncrement(by: target, ordering: .relaxed)
+            }
+        }
 
+        print("Part 2", part1 + part2.load(ordering: .relaxed))
     }
 
-    func solve(target: Int, operands: ArraySlice<Int>, accumulator: Int) -> Bool {
-        guard !operands.isEmpty else { return accumulator == target }
+    func solve(target: Int, operands: some RandomAccessCollection<Int>) -> Bool {
+        guard let operand = operands.first else { return target == 0 }
         let nextOperands = operands[operands.index(after: operands.startIndex)...]
 
-        let multiply = accumulator * operands[operands.startIndex]
-        let add = accumulator + operands[operands.startIndex]
+        if (target % operand) == 0 && solve(target: target / operand, operands: nextOperands) {
+            return true
+        } else if (target - operand) >= 0 && solve(target: target - operand, operands: nextOperands) {
+            return true
+        } else { return false }
+    }
 
-        if multiply <= target && solve(target: target, operands: nextOperands, accumulator: multiply) {
+    func solveWithConcatenation(target: Int, operands: some RandomAccessCollection<Int>) -> Bool {
+        guard let operand = operands.first else { return target == 0 }
+        let nextOperands = operands[operands.index(after: operands.startIndex)...]
+
+        if (target % operand) == 0 && solveWithConcatenation(target: target / operand, operands: nextOperands) {
             return true
-        } else if add <= target && solve(target: target, operands: nextOperands, accumulator: add) {
+        } else if (target - operand) >= 0 && solveWithConcatenation(target: target - operand, operands: nextOperands) {
             return true
+        } else if target > operand {
+            let magnitude = Int(
+                pow(
+                    10,
+                    ((log2(Double(operand)) / log2(10.0)) + 0.00001).rounded(.awayFromZero)
+                )
+            )
+
+            return (target % magnitude) == operand && solveWithConcatenation(target: target / magnitude, operands: nextOperands)
         } else {
             return false
         }
-    }
-    func solve2(target: Int, operands: ArraySlice<Int>, accumulator: Int) -> Bool {
-        guard !operands.isEmpty else { return accumulator == target }
-        let nextOperands = operands[operands.index(after: operands.startIndex)...]
-
-        let multiply = accumulator * operands[operands.startIndex]
-        let add = accumulator + operands[operands.startIndex]
-        let concat = accumulator || operands[operands.startIndex]
-
-        if multiply <= target && solve2(target: target, operands: nextOperands, accumulator: multiply) {
-            return true
-        } else if add <= target && solve2(target: target, operands: nextOperands, accumulator: add) {
-            return true
-        } else if concat <= target && solve2(target: target, operands: nextOperands, accumulator: concat) {
-            return true
-        } else {
-            return false
-        }
-    }
-
-}
-
-infix operator ||
-fileprivate func || (lhs: Int, rhs: Int) -> Int {
-    switch rhs {
-    case 0..<10: return lhs * 10 + rhs
-    case 10..<100: return lhs * 100 + rhs
-    case 100..<1000: return lhs * 1000 + rhs
-    case 1000..<10000: return lhs * 10000 + rhs
-    case 10000..<100000: return lhs * 100000 + rhs
-    case 100000..<1000000: return lhs * 1000000 + rhs
-    default: fatalError("Invalid operand")
     }
 }
